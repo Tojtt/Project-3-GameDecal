19920abf6a8ce062ea1456e8d4c71327c569dd49
Cyrus Hamirani <cyrushamirani@berkeley.edu>
2022-03-09 20:14:10 -0800

    functional task system prototype mostly in place, including UI + basic cobweb task

--- The-Apartment/Assets/Scripts/GameState.cs
+++ The-Apartment/Assets/Scripts/GameState.cs
@@ -4,30 +4,49 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
+    #region Possible Tasks for each day
+    [SerializeField]
+    GameObject[] tasks;
+    #endregion
+
     #region GameState_Variables
     // denotes whether all tasks are completed and can progress to the next day
     public bool dayFinished;
     // current day
     public int day;
+    private List<GameObject> taskQueue;
     #endregion
 
     private void Start()
     {
-        dayFinished = false;
-        day = 0;
+        taskQueue = new List<GameObject>();
+        PrepareNextDay(day);
     }
 
     #region Functions
     private void PrepareNextDay(int day)
     {
-
-        // at the end
+        taskQueue.Add(Instantiate(tasks[0]));
         dayFinished = false;
     }
 
-    private void CheckIfFinished()
+    /* The player sleeps and this function is called to advance the day. */
+    public void nextDay()
     {
+        if (taskQueue.Count == 0)
+        {
+            day += 1;
+            PrepareNextDay(day);
+        }
+    }
 
+    public GameObject getCurrentTask()
+    {
+        if (taskQueue.Count > 0)
+        {
+            return taskQueue[0];
+        }
+        return null;
     }
     #endregion
 }
