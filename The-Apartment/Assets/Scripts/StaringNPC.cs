2da39b8576452ce2b24b895a9bacab630a38958c
hhe195 <hhe195@berkeley.edu>
2022-03-31 15:29:36 -0700

    Created Staring person prefab

--- The-Apartment/Assets/Scripts/StaringNPC.cs
+++ The-Apartment/Assets/Scripts/StaringNPC.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 
-public class YarnInteractable : MonoBehaviour
+public class StaringNPC : MonoBehaviour
 {
+
+    #region Dialogue_variables
     private DialogueRunner dialogueRunner;
 
     private bool canInteract;
@@ -12,15 +14,52 @@ public class YarnInteractable : MonoBehaviour
     private bool isCurConvo;
 
     public int timesSpoken;
+    #endregion
 
+    #region Appearance_variables
+    public Sprite stareLeft;
+    public Sprite stareCenter;
+    public Sprite stareRight;
+    public int stare; //0 = Left, 1 = Center, 2 = Right
+    int halfRange = 1;
+    #endregion
+    GameObject player;
+
+    #region Unity_functions
     public void Start()
     {
         dialogueRunner = FindObjectOfType<DialogueRunner>();
         dialogueRunner.onDialogueComplete.AddListener(EndConversation);
         canInteract = true;
         timesSpoken = 0;
-
+        this.GetComponent<SpriteRenderer>().sprite = stareLeft; //Stares left
+        stare = 0; //Left
+        player = GameObject.Find("Player");
     }
+
+    public void Update()
+    {
+        double playerX = player.transform.position.x;
+        double x = transform.position.x;
+        if (playerX < x - halfRange) //Left
+        {
+            if (stare != 0) { 
+            this.GetComponent<SpriteRenderer>().sprite = stareLeft;
+            stare = 0;
+            }
+        } else if (playerX > x + halfRange) //Right
+        {
+            if (stare != 2) {
+                this.GetComponent<SpriteRenderer>().sprite = stareRight;
+                stare = 2;
+            }
+        } else if (stare != 1) //Center
+        {
+            this.GetComponent<SpriteRenderer>().sprite = stareCenter;
+            stare = 1;
+        }
+    }
+    #endregion
 
     // then we need a function to tell Yarn Spinner to start from {specifiedNodeName}
     public string conversationStartNode;
@@ -38,7 +77,7 @@ public class YarnInteractable : MonoBehaviour
         }
 
         dialogueRunner.StartDialogue(conversationStartNode);
-        timesSpoken++;
+        //timesSpoken++;
     }
 
     private void EndConversation()
