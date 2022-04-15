610964cd9d14c242c3bc888442d1304658533396
Cyrus Hamirani <cyrushamirani@berkeley.edu>
2022-04-14 17:25:13 -0700

    basi tutorial

--- The-Apartment/Assets/Scripts/Scenes & Gameplay/TutorialScript.cs
+++ The-Apartment/Assets/Scripts/Scenes & Gameplay/TutorialScript.cs
@@ -1,23 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Yarn.Unity;
 
 public class TutorialScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    GameObject playerChar;
-    GameObject friendChar;
+    public GameObject playerChar;
+    public GameObject friendChar;
+    private DialogueRunner dialogueRunner;
+    public LineView lineview;
+    private int tutorialProgress;
+    [SerializeField]
+    public List<String> tutorialNodes;
 
     void Start()
     {
         playerChar = GameObject.Find("Player");
         friendChar = GameObject.Find("Friend");
+
+        dialogueRunner = FindObjectOfType<DialogueRunner>();
+        lineview = FindObjectOfType<LineView>();
+
+        tutorialProgress = 0;
+        dialogueRunner.onDialogueComplete.AddListener(ProgressTutorial);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ProgressTutorial ()
+    {
+        dialogueRunner.Stop();
+        if (tutorialProgress < tutorialNodes.Count)
+        {
+            dialogueRunner.StartDialogue(tutorialNodes[tutorialProgress]);
+            tutorialProgress++;
+        }
+        else
+        {
+            SceneManager.LoadScene("Day1Scene");
+        }
     }
 }
